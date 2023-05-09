e589c39a1accab206eb144f4289590b2e4167bff
Stanislav <taix97@outlook.com>
2023-05-09 14:28:45 +0300

    added lever
    when you press the ball the other ball changes the color. It will be changed in the future to be triggered by the player

--- GXPEngine2023/GXPEngine/MyGame.cs
+++ GXPEngine2023/GXPEngine/MyGame.cs
@@ -2,40 +2,56 @@ using System;                                   // System contains a lot of defa
 using GXPEngine;                                // GXPEngine contains the engine
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
 
-public class MyGame : Game {
+public class MyGame : Game
+{
 
-	private Player player;
+    private Player player;
 
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		CreateLevel();
-		//// Draw some things on a canvas:
-		//EasyDraw canvas = new EasyDraw(800, 600);
-		//canvas.Clear(Color.MediumPurple);
-		//canvas.Fill(Color.Yellow);
-		//canvas.Ellipse(width / 2, height / 2, 200, 200);
-		//canvas.Fill(50);
-		//canvas.TextSize(32);
-		//canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		//canvas.Text("Welcome!", width / 2, height / 2);
+    private Lever lever;
+    private ResponsiveObject responsiveObject;
 
-		//// Add the canvas to the engine to display it:
-		//AddChild(canvas);
-		//Console.WriteLine("MyGame initialized");
-	}
+    public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
+    {
+        CreateLevel();
+        //// Draw some things on a canvas:
+        //EasyDraw canvas = new EasyDraw(800, 600);
+        //canvas.Clear(Color.MediumPurple);
+        //canvas.Fill(Color.Yellow);
+        //canvas.Ellipse(width / 2, height / 2, 200, 200);
+        //canvas.Fill(50);
+        //canvas.TextSize(32);
+        //canvas.TextAlign(CenterMode.Center, CenterMode.Center);
+        //canvas.Text("Welcome!", width / 2, height / 2);
 
-	private void CreateLevel() {
-		player = new Player(50, new Vec2(250,250));
-		AddChild(player);
-	}
-
-	// For every game object, Update is called every frame, by the engine:
-	void Update() {
-		player.Step();
+        //// Add the canvas to the engine to display it:
+        //AddChild(canvas);
+        //Console.WriteLine("MyGame initialized");
     }
 
-	static void Main()                          // Main() is the first method that's called when the program is run
-	{
-		new MyGame().Start();                   // Create a "MyGame" and start it
-	}
+    private void CreateLevel()
+    {
+        player = new Player(50, new Vec2(250, 250));
+
+        responsiveObject = new ResponsiveObject(30, new Vec2(10,10));
+        lever = new Lever(30, new Vec2(400, 300), responsiveObject);
+
+        AddChild(player);
+        AddChild(responsiveObject);
+        AddChild(lever);
+    }
+
+    // For every game object, Update is called every frame, by the engine:
+    void Update()
+    {
+        player.Step();
+        if (lever.IsMouseOver() && Input.GetMouseButtonDown(0))
+        {
+            lever.connectedObject.UpdateColor(152,242,0);
+        }
+    }
+
+    static void Main()                          // Main() is the first method that's called when the program is run
+    {
+        new MyGame().Start();                   // Create a "MyGame" and start it
+    }
 }
