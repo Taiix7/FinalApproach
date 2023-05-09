aaa9e71d5e60c26759d81660c919ea9c9c86cfce
Stanislav <taix97@outlook.com>
2023-05-09 12:03:53 +0300

    Added Lever

---
+++ GXPEngine2023/GXPEngine/Player.cs
@@ -0,0 +1,68 @@
+﻿using GXPEngine;
+using System.Collections.Generic;
+
+class Player : EasyDraw
+{
+    public int radius { get { return _radius; } }
+
+    public Vec2 position;
+
+    public float weight = .1f;
+
+    private int _radius;
+    private Vec2 velocity;
+    private Vec2 acceleration;
+
+    private Vec2 _oldPosition;
+
+    
+
+    public Player(int pRadius, Vec2 pPosition) : base(pRadius * 2 + 1, pRadius * 2 + 1)
+    {
+        this._radius = pRadius;
+        this.position = pPosition;
+
+        UpdateScreenPosition();
+        SetOrigin(_radius, _radius);
+        Draw(255, 255, 255);
+    }
+
+    void Draw(byte red, byte green, byte blue)
+    {
+        Fill(red, green, blue);
+        Stroke(red, green, blue);
+        Ellipse(_radius, _radius, 2 * _radius, 2 * _radius);
+    }
+    public void FollowMouse()
+    {
+        velocity += acceleration;
+        position += velocity;
+    }
+
+    private void UpdateScreenPosition()
+    {
+        x = position.x;
+        y = position.y;
+    }
+
+    void PlayerPhysics()
+    {
+        acceleration = new Vec2(0, weight);
+    }
+
+    public void Step()
+    {
+        FollowMouse();
+        PlayerPhysics();
+
+        velocity *= 0.99f;
+        _oldPosition = position;
+        UpdateScreenPosition();
+    }
+
+    void PlayerControl() {
+        if (Input.GetKey(Key.A)) { }
+    }
+
+}
+
