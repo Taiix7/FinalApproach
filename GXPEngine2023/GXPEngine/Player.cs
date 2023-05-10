1d2f516ccc09a4caaa1e2f361542439970ab0825
Adrian Georgiev <andigebg@gmail.com>
2023-05-10 12:48:47 +0200

    Added custom wall collision

--- GXPEngine2023/GXPEngine/Player.cs
+++ GXPEngine2023/GXPEngine/Player.cs
@@ -10,11 +10,14 @@ class Player : EasyDraw
 
     private int _radius;
 
+    public float difline;
+    public GameObject latestCollision = null;
+
     private float speed = 1f;
     private float weight = .01f;
     private float jumpSpeed = 5;
 
-    private Vec2 velocity;
+    public Vec2 velocity;
     private Vec2 acceleration;
     private Vec2 _oldPosition;
 
@@ -43,7 +46,7 @@ class Player : EasyDraw
         position += velocity;
     }
 
-    private void UpdateScreenPosition()
+    public void UpdateScreenPosition()
     {
         x = position.x;
         y = position.y;
@@ -59,6 +62,7 @@ class Player : EasyDraw
     {
         PlayerPhysics();
         PlayerControl();
+        CheckCollision();
 
         velocity *= 0.99f;
         _oldPosition = position;
@@ -78,5 +82,115 @@ class Player : EasyDraw
     }
 
     public void Land() { canJump = true; }
+
+    public float TOIBallLine(Player move, LineSegment line, Vec2 difference, bool top)
+    {
+        float t = 10;
+        Vec2 lineLenght = (line.end - line.start);
+        Vec2 lineNormal = lineLenght.Normal();
+        Vec2 lineNormalized = lineLenght.Normalized();
+
+        float a = difference.Dot(lineNormal) - move.radius;
+        float b = -lineNormal.Dot(move.velocity);
+
+        if (b <= 0) return 10;
+
+
+        if (a >= 0)
+        {
+            t = a / b;
+        }
+        else if (a >= -move.radius)
+        {
+            t = 0;
+        }
+        else return 10;
+
+
+        if (t < 1)
+        {
+            Vec2 POI = _oldPosition + velocity * t;
+            Vec2 newDif = POI - line.start;
+
+            float d = newDif.Dot(lineNormalized);
+
+            if (d >= 0 && d <= lineLenght.Length()) return t;
+            else return 10;
+
+        }
+        else return 10;
+
+
+    }
+
+    public void CheckCollision()
+    {
+        CollisionInfo firstCollision = FindEarliestCollision();
+        if (firstCollision != null)
+        {
+            ResolveCollision(firstCollision);
+        }
+        else
+            UpdateScreenPosition();
+    }
+
+    CollisionInfo FindEarliestCollision()
+    {
+        MyGame myGame = (MyGame)game;
+
+        GameObject otherCol = null;
+
+        Vec2 firstnormal = new Vec2(0, 1);
+        float FirstTOI = 2;
+
+
+        // Check other movers:
+        foreach (LineSegment lines in myGame.list)
+        {
+
+            Vec2 point = lines.start;
+            Vec2 line = lines.start - lines.end;
+
+            Vec2 normalLine = line.Normal();
+            Vec2 difference = point - position;
+
+            Vec2 oldDif = _oldPosition - point;
+
+            float ballDistance = difference.Dot(normalLine);
+
+            if (Mathf.Abs(ballDistance) <= radius)
+            {
+                float t = TOIBallLine(this, lines, oldDif, false);
+
+                if (t < FirstTOI)
+                {
+                    firstnormal = normalLine;
+                    otherCol = lines;
+                    FirstTOI = t;
+                    difline = ballDistance;
+
+                }
+            }
+        }
+        
+
+        if (FirstTOI < 1)
+        {
+            return new CollisionInfo(firstnormal, otherCol, FirstTOI);
+        }
+        else return null;
+    }
+
+    void ResolveCollision(CollisionInfo col)
+    {
+        latestCollision = col.other;
+        MyGame myGame = (MyGame)game;
+        if (col.other is NLineSegment)
+        {
+            position -= (-difline + radius) * col.normal;
+            velocity.Reflect(col.normal);
+        }
+    }
+
 }
 
